13f7b02956c965e6f8ea3327011e20b8ff9fd036
Matt Priour <mpriour@opengeo.org>
2012-05-07 14:38:01 +0200

    Update layer model with jsdoc tags and proper class name & alias

--- src/GeoExt/data/models/Layer.js
+++ src/GeoExt/data/models/Layer.js
@@ -1,9 +1,23 @@
-Ext.define('gx_layer', {
+/**
+ * @class GeoExt.data.model.Layer
+ * @borrows Ext.data.Model
+ * 
+ *  Class defines a model for records containing an OpenLayers layer object.
+ *  Usually this class is not instantiated directly, but referenced by its mtype 'gx_layer' 
+ *  or name 'GeoExt.data.model.Layer' as string representation as a config option within creation of a superior component, 
+ *  such as a store.
+ */
+Ext.define('GeoExt.data.model.Layer', {
     extend: 'Ext.data.Model',
+    alias: ['model.gx_layer'],
     fields: [
-        'name',
+        {name: 'title', mapping: 'name'},
+        {name: 'name', mapping: 'metadata.name'},
         'id'
     ],
+/**
+ * Returns the {OpenLayers.Layer} layer object used in this model instance
+ */
     getLayer: function() {
         return this.raw;
     }
