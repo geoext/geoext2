f87ad80c5f07dc0ddf862660fdb1b214003ca2d7
François Van Der Biest <francois.vanderbiest@gmail.com>
2012-05-09 13:38:49 +0200

    attribute model: now takes into account the GeoExt.data.reader.Attribute

--- src/GeoExt/data/AttributeModel.js
+++ src/GeoExt/data/AttributeModel.js
@@ -1,22 +1,39 @@
-Ext.define('GeoExt.data.AttributeModel',{
+/**
+ * Copyright (c) 2008-2012 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license.
+ * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
+ * of the license.
+ */
+
+/**
+ * @requires GeoExt/data/reader/Attribute.js
+ */
+
+/**
+ * @class GeoExt.data.AttributeModel
+ * A specific model, which is fit for the records created by the 
+ * {@link GeoExt.data.reader.Attribute}'s readRecords method.
+ */
+Ext.define('GeoExt.data.AttributeModel', {
     alternateClassName: 'GeoExt.data.AttributeRecord',
     extend: 'Ext.data.Model',
-    requires: ['Ext.data.proxy.Ajax'], // later on: 'GeoExt.data.reader.Attribute'],
+    requires: ['Ext.data.proxy.Ajax', 'GeoExt.data.reader.Attribute'],
     alias: 'model.gx_attribute',
     fields: [
         {name: 'name', mapping: 'name'},
         {name: 'type', mapping: 'type'},
         {name: 'restriction', mapping: 'restriction'},
         {name: 'nillable', type: 'bool', mapping: 'nillable'}
-        // not always a 'value' field 
+        // No 'value' field by default. 
+        // It gets added by the GeoExt.data.reader.Attribute constructor 
+        // if it is given a feature.
     ],
     proxy: {
         type: 'ajax',
-        url: 'change_me' // should not be not empty or it fails in the tests
-        // later on:
-        /*,
+        url: 'change_me', // should not be not empty or it fails in the tests,
         reader: {
             type: 'gx_attribute'
-        }*/
+        }
     }
 });
