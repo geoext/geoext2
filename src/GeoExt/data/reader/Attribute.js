e174a899f9cc343a2570d30910802bbd09e29894
Éric Lemoine <eric.lemoine@gmail.com>
2012-05-09 12:53:17 +0200

    add an Attribute Reader

---
+++ src/GeoExt/data/reader/Attribute.js
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2008-2011 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license.
+ * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
+ * of the license.
+ */
+
+/**
+ * @require OpenLayers/Format/WFSDescribeFeatureType.js
+ */
+
+/* api: (define)
+ *  module = GeoExt.data
+ *  class = AttributeReader
+ *  base_link = `Ext.data.DataReader <http://dev.sencha.com/deploy/dev/docs/?class=Ext.data.DataReader>`_
+ */
+
+Ext.define('GeoExt.data.reader.Attribute', {
+    extend: 'Ext.data.reader.Json',
+    requires: ['Ext.data.reader.Json', 'Ext.data.Field'],
+    alternateClassName: 'GeoExt.data.AttributeReader',
+    alias: 'reader.gx_attribute',
+
+    /** api: constructor
+     *  .. class:: AttributeReader(meta, recordType)
+     *  
+     *      :arg meta: ``Object`` Reader configuration.
+     *      :arg recordType: ``Array or Ext.data.Record`` An array of field
+     *          configuration objects or a record object.
+     *
+     *      Create a new attributes reader object.
+     *      
+     *      Valid meta properties:
+     *      
+     *      * format - ``OpenLayers.Format`` A parser for transforming the XHR response
+     *        into an array of objects representing attributes.  Defaults to
+     *        an ``OpenLayers.Format.WFSDescribeFeatureType`` parser.
+     *      * ignore - ``Object`` Properties of the ignore object should be field names.
+     *        Values are either arrays or regular expressions.
+     *      * feature - ``OpenLayers.Feature.Vector`` A vector feature. If provided
+     *        records created by the reader will include a field named "value"
+     *        referencing the attribute value as set in the feature.
+     */
+    constructor: function(config) {
+        config = config || {};
+        if(!config.format) {
+            config.format = new OpenLayers.Format.WFSDescribeFeatureType();
+        }
+        this.callParent([config]);
+        // FIXME config.fields?
+        //GeoExt.data.AttributeReader.superclass.constructor.call(
+            //this, config, recordType || config.fields
+        //);
+        if(config.feature) {
+            var f = Ext.create('Ext.data.Field', {
+                name: "value",
+                defaultValue: undefined // defaultValue defaults to ""
+                                        // in Ext.data.Field, we may 
+                                        // need to change that...
+            });
+            this.model.prototype.fields.add(f);
+        }
+    },
+
+    /** private: method[read]
+     *  :arg request: ``Object`` The XHR object that contains the parsed doc.
+     *  :return: ``Object``  A data block which is used by an ``Ext.data.Store``
+     *      as a cache of ``Ext.data.Records``.
+     *  
+     *  This method is only used by a DataProxy which has retrieved data from a
+     *  remote server.
+     */
+    read: function(request) {
+              // FIXME remove?
+        var data = request.responseXML;
+        if(!data || !data.documentElement) {
+            data = request.responseText;
+        }
+        return this.readRecords(data);
+    },
+
+    /** private: method[readRecords]
+     *  :arg data: ``DOMElement or String or Array`` A document element or XHR
+     *      response string.  As an alternative to fetching attributes data from
+     *      a remote source, an array of attribute objects can be provided given
+     *      that the properties of each attribute object map to a provided field
+     *      name.
+     *  :return: ``Object`` A data block which is used by an ``Ext.data.Store``
+     *      as a cache of ``Ext.data.Records``.
+     *  
+     *  Create a data block containing Ext.data.Records from an XML document.
+     */
+    readRecords: function(data) {
+        var attributes;
+        if(data instanceof Array) {
+            attributes = data;
+        } else {
+            // only works with one featureType in the doc
+            attributes = this.format.read(data).featureTypes[0].properties;
+        }
+        var feature = this.feature;
+        var Model = this.model;
+        var fields = Model.prototype.fields;
+        var numFields = fields.length;
+        var attr, values, name, record, ignore, value, field, records = [];
+        for(var i=0, len=attributes.length; i<len; ++i) {
+            ignore = false;
+            attr = attributes[i];
+            values = {};
+            for(var j=0; j<numFields; ++j) {
+                field = fields.items[j];
+                name = field.name;
+                if (field.convert) {
+                    value = field.convert(attr[name]);
+                } else {
+                    value = attr[name];
+                }
+                if(this.ignoreAttribute(name, value)) {
+                    ignore = true;
+                    break;
+                }
+                values[name] = value;
+            }
+            if(feature) {
+                value = feature.attributes[values.name];
+                if(value !== undefined) {
+                    if(this.ignoreAttribute("value", value)) {
+                        ignore = true;
+                    } else {
+                        values.value = value;
+                    }
+                }
+            }
+            if(!ignore) {
+                records[records.length] = new Model(values);
+            }
+        }
+
+        return new Ext.data.ResultSet({
+            success: true,
+            records: records,
+            count: records.length,
+            total: records.length,
+            message: ''
+        });
+    },
+
+    /** private: method[ignoreAttribute]
+     *  :arg name: ``String`` The field name.
+     *  :arg value: ``String`` The field value.
+     *
+     *  :return: ``Boolean`` true if the attribute should be ignored.
+     */
+    ignoreAttribute: function(name, value) {
+        var ignore = false;
+        if(this.ignore && this.ignore[name]) {
+            var matches = this.ignore[name];
+            if(typeof matches == "string") {
+                ignore = (matches === value);
+            } else if(matches instanceof Array) {
+                ignore = (matches.indexOf(value) > -1);
+            } else if(matches instanceof RegExp) {
+                ignore = (matches.test(value));
+            }
+        }
+        return ignore;
+    }
+});
