d2f63bb137fb6d122c0602c5d14d9e3570e4d145
Markus Schneider <schneider@occamlabs.de>
2012-05-09 14:39:51 +0200

    Documentation warning fixes

--- src/GeoExt/data/LayerStore.js
+++ src/GeoExt/data/LayerStore.js
@@ -1,5 +1,4 @@
 /**
- * @requires GeoExt/data/LayerModel.js
  * @class GeoExt.data.LayerStore
  * A store that synchronizes a layers array of an OpenLayers.Map with a
  * layer store holding {@link GeoExt.data.LayerModel} instances.
