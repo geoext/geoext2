260e98d65f499d33a55e137b85967a76bd371d13
Matt Priour <mpriour@opengeo.org>
2012-05-09 09:41:24 +0200

    Adding advanced native setter/getter functionality back to the layer model

--- src/GeoExt/data/LayerModel.js
+++ src/GeoExt/data/LayerModel.js
@@ -1,3 +1,12 @@
+/**
+ * @class GeoExt.data.LayerModel
+ * @borrows Ext.data.Model
+ * 
+ *  Class defines a model for records containing an OpenLayers layer object.
+ *  Usually this class is not instantiated directly, but referenced by its mtype 'gx_layer' 
+ *  or name 'GeoExt.data.model.Layer' as string representation as a config option within creation of a superior component, 
+ *  such as a store.
+ */
 Ext.define('GeoExt.data.LayerModel',{
     alternateClassName: 'GeoExt.data.LayerRecord',
     extend: 'Ext.data.Model',
@@ -5,10 +14,15 @@ Ext.define('GeoExt.data.LayerModel',{
     alias: 'model.gx_layer',
     fields: [   
         'id',
-        {name: 'title', type: 'string', mapping: 'name'},
-        {name: 'legendURL', type: 'string', mapping: 'metadata.legendURL'},
-        {name: 'hideTitle', type: 'bool', mapping: 'metadata.hideTitle'},
-        {name: 'hideInLegend', type: 'bool', mapping: 'metadata.hideInLegend'}
+        {name: 'title',        type: 'string', mapping: 'name'},
+        {name: 'legendURL',    type: 'string', mapping: 'metadata.legendURL'},
+        {name: 'hideTitle',    type: 'bool',   mapping: 'metadata.hideTitle'},
+        {name: 'hideInLegend', type: 'bool',   mapping: 'metadata.hideInLegend'},
+        {name: 'opacity',      type: 'float'},
+        {name: 'zIndex',       type: 'int'},       
+        {name: 'isBaseLayer',  type: 'bool'}, 
+        {name: 'visibility',   type: 'bool'},     
+        {name: 'attribution',  type: 'string'}
     ],
     proxy: {
         type: 'memory',
@@ -21,5 +35,63 @@ Ext.define('GeoExt.data.LayerModel',{
      */
     getLayer: function() {
         return this.raw;
+    },
+    
+    /**
+     * @private
+     */
+    constructor: function(data, id, raw, convertedData) {
+        var me = this,
+            layer = raw;
+        me.callParent(arguments);
+        //attach listeners to layer properties modification events      
+        layer.events.on({
+            'visibilitychanged':function(evt){
+                var visiblity = evt.object.getVisibility();
+                this[this.persistenceProperty]['visibility'] = visibility;
+                Ext.Array.include(this.modified,'visibility');
+                this.dirty = true;
+                this.callStore('afterEdit', this.modified);
+            },
+            scope: this
+        });
+
+    },
+    /**
+     * @private
+     */    
+    afterEdit: function(fields){
+        var layer = this.raw;
+        for(var i=0,len=fields.length;i<len;i++){
+            var field = fields[i];
+            var nativeFunc = this.findNativeAccessor('set',field);
+            if(nativeFunc){
+                nativeFunc(this.data.field);
+            }
+        }
+        this.callParent([fields]);
+    },
+    
+    //inherit docs
+    get: function(field){
+        var val;
+        var nativeFunc = this.findNativeAccessor('get',field.name || field);
+        if(nativeFunc){
+            val = nativeFunc(this.data.field);
+        } else {
+            //no native getter, so just use the normal model get function
+            //Ext.data.Model doesn't contain post processing code, so don't callParent otherwise
+            val = this.callParent([field]);
+        }
+        return val;
+    },
+    /**
+     * @private
+     */
+    findNativeAccessor: function(operation,property){
+        var layer = this.raw;
+        var nativeProp = (this.fields.get(property) && this.fields.get(property).mapping) || property;
+        var nativeFunc = layer[operation+nativeProp[0].toUpperCase()+nativeProp.slice(1)];
+        return nativeFunc | null;
     }
 });
