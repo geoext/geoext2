3df2c56c108dd356d14088e0b840410d7a914a61
Matt Priour <mpriour@opengeo.org>
2012-05-09 10:58:01 +0200

    initial capabilities layer model definition

---
+++ src/GeoExt/data/WmsCapabilitiesLayerModel.js
@@ -0,0 +1,37 @@
+Ext.define('GeoExt.data.WmsCapabilitiesLayerModel',{
+    extend: 'GeoExt.data.LayerModel',
+    alternateClassName: ['GeoExt.data.WMSCapabilitiesModel','GeoExt.data.WmsCapabilitiesModel'],
+    alias: 'model.gx_wmscapabilities',
+    fields: [
+                {name: "name", type: "string", mapping: "metadata.name"},
+                {name: "title", type: "string", mapping: "metadata.title"},
+                {name: "abstract", type: "string", mapping: "metadata.abstract"},
+                {name: "queryable", type: "boolean", mapping: "metadata.queryable"},
+                {name: "opaque", type: "boolean", mapping: "metadata.opaque"},
+                {name: "noSubsets", type: "boolean", mapping: "metadata.noSubsets"},
+                {name: "cascaded", type: "int", mapping: "metadata.casacaded"},
+                {name: "fixedWidth", type: "int", mapping: "metadata.fixedWidth"},
+                {name: "fixedHeight", type: "int", mapping: "metadata.fixedHeight"},
+                {name: "minScale", type: "float", mapping: "metadata.minScale"},
+                {name: "maxScale", type: "float", mapping: "metadata.maxScale"},
+                {name: "prefix", type: "string", mapping: "metadata.prefix"},
+                {name: "formats", mapping: "metadata.formats"}, // array
+                {name: "styles", mapping: "metadata.styles"}, // array
+                {name: "srs", mapping: "metadata.srs"}, // object
+                {name: "dimensions", mapping: "metadata.dimensions"}, // object
+                {name: "bbox", mapping: "metadata.bbox"}, // object
+                {name: "llbbox", mapping: "metadata.llbox"}, // array
+                {name: "attribution", type: "string"},
+                {name: "keywords", mapping: "metadata.keywords"}, // array
+                {name: "identifiers", mapping: "metadata.identifiers"}, // object
+                {name: "authorityURLs", mapping: "metadata.authorityURLs"}, // object
+                {name: "metadataURLs", mapping: "metadata.metadataURLs"} // array    
+    ],
+    proxy: {
+        type: 'ajax',
+        reader: {
+            type: 'gx_wmscapabilites'
+        }
+    }
+});
+
