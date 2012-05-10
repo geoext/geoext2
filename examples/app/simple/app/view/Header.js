2901a5e825a21eb32c43a82420f50d01cadbe9b0
Alexandre Dube <adube@mapgears.com>
2012-05-10 14:06:07 +0200

    examples - new app/simple, only using Ext for now

---
+++ examples/app/simple/app/view/Header.js
@@ -0,0 +1,18 @@
+/**
+ * The application header displayed at the top of the viewport
+ * @extends Ext.Component
+ */
+Ext.define('GX.view.Header', {
+    extend: 'Ext.Component',
+    
+    dock: 'top',
+    baseCls: 'app-header',
+    
+    initComponent: function() {
+        Ext.applyIf(this, {
+            html: 'MVC simple application example called GX'
+        });
+                
+        this.callParent(arguments);
+    }
+});
