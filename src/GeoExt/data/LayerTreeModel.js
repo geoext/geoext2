de210618e201ad7e8694282f3f771cb27d055934
ahocevar <andreas.hocevar@gmail.com>
2012-05-10 17:13:15 +0200

    Store plugin properties on the record.

--- src/GeoExt/data/LayerTreeModel.js
+++ src/GeoExt/data/LayerTreeModel.js
@@ -1,3 +1,7 @@
+/**
+ * Model used for trees that use GeoExt tree components.
+ * @class
+ */
 Ext.define('GeoExt.data.LayerTreeModel',{
     alternateClassName: 'GeoExt.data.LayerTreeRecord',
     extend: 'Ext.data.Model',
@@ -6,10 +10,14 @@ Ext.define('GeoExt.data.LayerTreeModel',{
     fields: [  
         {name: 'text', type: 'string'}, 
         {name: 'plugins'},
+        {name: 'layer'},
         {name: 'checkedGroup', type: 'string'},
-        {name: 'layer'}
+        {name: 'fixedText', type: 'bool'}
     ],
     
+    /**
+     * @private
+     */
     init: function() {
         var me = this;
 
