f54e6a5b1160d82e53b94a31d1a53fd2cd1fa074
Christian Mayer <mayer@terrestris.de>
2012-05-10 14:01:46 +0200

    Ported the PrintExtent class, example is running, still some adaptions needed (API-Docs, remove new-instanciation...)

---
+++ examples/printextent/print-extent.js
@@ -0,0 +1,45 @@
+Ext.require([
+    'Ext.Window',
+    'GeoExt.panel.Map',
+    'GeoExt.data.PrintProvider',
+    'GeoExt.panel.PrintMap',
+    'GeoExt.plugins.PrintExtent'
+]);
+
+var mapPanel, printProvider;
+
+Ext.onReady(function() {
+    // The printProvider that connects us to the print service
+    printProvider = new GeoExt.data.PrintProvider({
+        method: "GET", // "POST" recommended for production use
+        capabilities: printCapabilities, // from the info.json script in the html
+        customParams: {
+            mapTitle: "Printing Demo",
+            comment: "This is a map printed from GeoExt."
+        }
+    });
+
+    var printExtent = new GeoExt.plugins.PrintExtent({
+        printProvider: printProvider
+    });
+
+    // The map we want to print, with the PrintExtent added as item.
+    mapPanel = new GeoExt.MapPanel({
+        renderTo: "content",
+        width: 450,
+        height: 320,
+        layers: [new OpenLayers.Layer.WMS("Tasmania", "http://demo.opengeo.org/geoserver/wms",
+            {layers: "topp:tasmania_state_boundaries"}, {singleTile: true})],
+        center: [146.56, -41.56],
+        zoom: 6,
+        plugins: [printExtent],
+        bbar: [{
+            text: "Create PDF",
+            handler: function() {
+                // the PrintExtent plugin is the mapPanel's 1st plugin
+                mapPanel.plugins[0].print();
+            }
+        }]
+    });
+    printExtent.addPage();
+});
