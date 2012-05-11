78e164a6d305c318bad0d2b0d85637e89bb849eb
Éric Lemoine <eric.lemoine@gmail.com>
2012-05-11 12:21:50 +0200

    Basic Form - use jsdoc

--- src/GeoExt/form/Basic.js
+++ src/GeoExt/form/Basic.js
@@ -1,66 +1,58 @@
-/**
- * Copyright (c) 2008-2011 The Open Source Geospatial Foundation
+/*
+ * Copyright (c) 2008-2012 The Open Source Geospatial Foundation
  * 
  * Published under the BSD license.
  * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
  * of the license.
  */
 
+/*
+ * @include GeoExt/widgets/form/action/Search.js
+ */
+
 /**
- * @include GeoExt/widgets/form/SearchAction.js
- */
-
-/** api: (define)
- *  module = GeoExt.form
- *  class = BasicForm
- *  base_link = `Ext.form.BasicForm <http://dev.sencha.com/deploy/dev/docs/?class=Ext.form.BasicForm>`_
- */
-
-Ext.namespace("GeoExt.form");
-
-/** api: constructor
- *  .. class:: BasicForm(config)
+ * A specific `Ext.form.Basic` whose `doAction` method creates
+ * a {@link GeoExt.form.action.Search} if it is passed the string
+ * "search" as its first argument.
  *
- *      A specific ``Ext.form.BasicForm`` whose doAction method creates
- *      a :class:`GeoExt.form.SearchAction` if it is passed the string
- *      "search" as its first argument.
- *
- *      In most cases one would not use this class directly, but
- *      :class:`GeoExt.form.FormPanel` instead.
+ * In most cases one would not use this class directly, but
+ * `GeoExt.form.Panel` instead.
  */
 Ext.define('GeoExt.form.Basic', {
     extend: 'Ext.form.Basic',
     requires: ['GeoExt.form.action.Search'],
 
-    /** private: property[protocol]
-     *  ``OpenLayers.Protocol`` The protocol configured in this
-     *  instance.
+    /**
+     * @cfg {OpenLayers.Protocol} protocol
+     * The protocol to use for search requests.
      */
-    protocol: null,
+    /**
+     * @property {OpenLayers.Protocol} protocol
+     * The protocol.
+     */
 
     /**
-     * private: property[prevResponse]
-     * ``OpenLayers.Protocol.Response`` The response return by a call to
-     *  protocol.read method.
+     * @property {OpenLayers.Protocol.Response} prevResponse
+     * The response return by a call to  protocol.read method.
+     * @private
      */
-    prevResponse: null,
 
     /**
-     * api: config[autoAbort]
-     * ``Boolean`` Tells if pending requests should be aborted
-     *      when a new action is performed.
+     * @cfg {Boolean}
+     * Tells if pending requests should be aborted when a new action
+     * is performed. Default is `true`.
      */
     autoAbort: true,
 
-    /** api: method[doAction]
-     *  :param action: ``String or Ext.form.Action`` Either the name
-     *      of the action or a ``Ext.form.Action`` instance.
-     *  :param options: ``Object`` The options passed to the Action
-     *      constructor.
-     *  :return: :class:`GeoExt.form.BasicForm` This form.
+    /**
+     * Performs the action, if the string "search" is passed as the
+     * first argument then a {@link GeoExt.form.action.Search} is created.
+     * @param {String/Ext.form.action.Action} action Either the name
+     * of the action or a `Ext.form.action.Action` instance.
+     * @param {Object} options The options passed to the Action
+     * constructor.
+     * @return {GeoExt.form.Basic} This form.
      *
-     *  Performs the action, if the string "search" is passed as the
-     *  first argument then a :class:`GeoExt.form.SearchAction` is created.
      */
     doAction: function(action, options) {
         if(action == "search") {
@@ -74,12 +66,12 @@ Ext.define('GeoExt.form.Basic', {
         return this.callParent([action, options]);
     },
 
-    /** api: method[search]
-     *  :param options: ``Object`` The options passed to the Action
-     *      constructor.
-     *  :return: :class:`GeoExt.form.BasicForm` This form.
+    /**
+     * Shortcut to do a search action.
+     * @param {Object} options The options passed to the Action
+     * constructor.
+     * @return {GeoExt.form.Basic} This form.
      *  
-     *  Shortcut to do a search action.
      */
     search: function(options) {
         return this.doAction("search", options);
