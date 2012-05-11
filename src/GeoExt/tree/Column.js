4e3057a65e9337d5cd2680a8b369367cccb55ccd
François Van Der Biest <francois.vanderbiest@gmail.com>
2012-05-11 11:00:30 +0200

    doc - fixed missing or incorrect copyright strings

--- src/GeoExt/tree/Column.js
+++ src/GeoExt/tree/Column.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2008-2012 The Open Source Geospatial Foundation
+ *
+ * Published under the BSD license.
+ * See https://github.com/geoext/geoext2/blob/master/license.txt for the full text
+ * of the license.
+ */
+
 /**
  * Provides indentation and folder structure markup for a Tree taking into account
  * depth and position within the tree hierarchy.
