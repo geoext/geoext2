66f1866ae072dd0eb92e19a542cf00ff04e3a7a5
Éric Lemoine <eric.lemoine@gmail.com>
2012-05-12 20:46:37 +0200

    fix indent in LayerStore.js

--- src/GeoExt/data/LayerStore.js
+++ src/GeoExt/data/LayerStore.js
@@ -180,223 +180,223 @@ Ext.define('GeoExt.data.LayerStore', {
         }
     },
 
-        /**
-         * Handler for layer changes.  When layer order changes, this moves the
-         * appropriate record within the store.
-         * @private
-         * @param {Object} evt
-         */
-        onChangeLayer: function(evt) {
-            var layer = evt.layer;
-            var recordIndex = this.findBy(function(rec, id) {
-                return rec.getLayer() === layer;
-            });
-            if(recordIndex > -1) {
-                var record = this.getAt(recordIndex);
-                if(evt.property === "order") {
-                    if(!this._adding && !this._removing) {
-                        var layerIndex = this.map.getLayerIndex(layer);
-                        if(layerIndex !== recordIndex) {
-                            this._removing = true;
-                            this.remove(record);
-                            delete this._removing;
-                            this._adding = true;
-                            this.insert(layerIndex, [record]);
-                            delete this._adding;
-                        }
+    /**
+     * Handler for layer changes.  When layer order changes, this moves the
+     * appropriate record within the store.
+     * @private
+     * @param {Object} evt
+     */
+    onChangeLayer: function(evt) {
+        var layer = evt.layer;
+        var recordIndex = this.findBy(function(rec, id) {
+            return rec.getLayer() === layer;
+        });
+        if(recordIndex > -1) {
+            var record = this.getAt(recordIndex);
+            if(evt.property === "order") {
+                if(!this._adding && !this._removing) {
+                    var layerIndex = this.map.getLayerIndex(layer);
+                    if(layerIndex !== recordIndex) {
+                        this._removing = true;
+                        this.remove(record);
+                        delete this._removing;
+                        this._adding = true;
+                        this.insert(layerIndex, [record]);
+                        delete this._adding;
                     }
-                } else if(evt.property === "name") {
-                    record.set("title", layer.name);
-                } else {
-                    this.fireEvent("update", this, record, Ext.data.Record.EDIT);
                 }
-            }
-        },
-
-        /**
-         * Handler for a map's addlayer event
-         * @private
-         * @param {Object} evt
-         */
-        onAddLayer: function(evt) {
-            var me = this;
-            if(!me._adding) {
-                me._adding = true;
-                var result  = me.proxy.reader.read(evt.layer);
-                me.add(result.records);
-                delete me._adding;
-            }
-        },
-
-        /**
-         * Handler for a map's removelayer event
-         * @private
-         * @param {Object} evt
-         */
-        onRemoveLayer: function(evt){
-            //TODO replace the check for undloadDestroy with a listener for the
-            // map's beforedestroy event, doing unbind(). This can be done as soon
-            // as http://trac.openlayers.org/ticket/2136 is fixed.
-            if(this.map.unloadDestroy) {
-                if(!this._removing) {
-                    var layer = evt.layer;
-                    this._removing = true;
-                    this.remove(this.getByLayer(layer));
-                    delete this._removing;
-                }
+            } else if(evt.property === "name") {
+                record.set("title", layer.name);
             } else {
-                this.unbind();
+                this.fireEvent("update", this, record, Ext.data.Record.EDIT);
             }
-        },
+        }
+    },
 
-        /**
-         * Handler for a store's load event
-         * @private
-         * @param {Ext.data.Store} store
-         * @param {Ext.data.Model[]} records
-         * @param {Boolean} successful
-         */
-        onLoad: function(store, records, successful) {
-            if (successful) {
-                if (!Ext.isArray(records)) {
-                    records = [records];
-                }
+    /**
+     * Handler for a map's addlayer event
+     * @private
+     * @param {Object} evt
+     */
+    onAddLayer: function(evt) {
+        var me = this;
+        if(!me._adding) {
+            me._adding = true;
+            var result  = me.proxy.reader.read(evt.layer);
+            me.add(result.records);
+            delete me._adding;
+        }
+    },
+
+    /**
+     * Handler for a map's removelayer event
+     * @private
+     * @param {Object} evt
+     */
+    onRemoveLayer: function(evt){
+        //TODO replace the check for undloadDestroy with a listener for the
+        // map's beforedestroy event, doing unbind(). This can be done as soon
+        // as http://trac.openlayers.org/ticket/2136 is fixed.
+        if(this.map.unloadDestroy) {
+            if(!this._removing) {
+                var layer = evt.layer;
                 this._removing = true;
-                for (var i = this.map.layers.length - 1; i >= 0; i--) {
-                    this.map.removeLayer(this.map.layers[i]);
-                }
+                this.remove(this.getByLayer(layer));
                 delete this._removing;
-
-                // layers has already been added to map on "add" event
-                var len = records.length;
-                if (len > 0) {
-                    var layers = new Array(len);
-                    for (var j = 0; j < len; j++) {
-                        layers[j] = records[j].getLayer();
-                    }
-                    this._adding = true;
-                    this.map.addLayers(layers);
-                    delete this._adding;
-                }
             }
-        },
+        } else {
+            this.unbind();
+        }
+    },
 
-        /**
-         * Handler for a store's clear event
-         * @private
-         * @param {Ext.data.Store} store
-         */
-        onClear: function(store) {
+    /**
+     * Handler for a store's load event
+     * @private
+     * @param {Ext.data.Store} store
+     * @param {Ext.data.Model[]} records
+     * @param {Boolean} successful
+     */
+    onLoad: function(store, records, successful) {
+        if (successful) {
+            if (!Ext.isArray(records)) {
+                records = [records];
+            }
             this._removing = true;
             for (var i = this.map.layers.length - 1; i >= 0; i--) {
                 this.map.removeLayer(this.map.layers[i]);
             }
             delete this._removing;
-        },
 
-        /**
-         * Handler for a store's add event
-         * @private
-         * @param {Ext.data.Store} store
-         * @param {Ext.data.Model[]} records
-         * @param {Number} index
-         */
-        onAdd: function(store, records, index) {
-            if(!this._adding) {
-                this._adding = true;
-                var layer;
-                for(var i=records.length-1; i>=0; --i) {
-                    layer = records[i].getLayer();
-                    this.map.addLayer(layer);
-                    if(index !== this.map.layers.length-1) {
-                        this.map.setLayerIndex(layer, index);
-                    }
+            // layers has already been added to map on "add" event
+            var len = records.length;
+            if (len > 0) {
+                var layers = new Array(len);
+                for (var j = 0; j < len; j++) {
+                    layers[j] = records[j].getLayer();
                 }
+                this._adding = true;
+                this.map.addLayers(layers);
                 delete this._adding;
             }
-        },
+        }
+    },
 
-        /**
-         * Handler for a store's remove event
-         * @private
-         * @param {Ext.data.Store} store
-         * @param {Ext.data.Model} record
-         * @param {Number} index
-         */
-        onRemove: function(store, record, index){
-            if(!this._removing) {
+    /**
+     * Handler for a store's clear event
+     * @private
+     * @param {Ext.data.Store} store
+     */
+    onClear: function(store) {
+        this._removing = true;
+        for (var i = this.map.layers.length - 1; i >= 0; i--) {
+            this.map.removeLayer(this.map.layers[i]);
+        }
+        delete this._removing;
+    },
+
+    /**
+     * Handler for a store's add event
+     * @private
+     * @param {Ext.data.Store} store
+     * @param {Ext.data.Model[]} records
+     * @param {Number} index
+     */
+    onAdd: function(store, records, index) {
+        if(!this._adding) {
+            this._adding = true;
+            var layer;
+            for(var i=records.length-1; i>=0; --i) {
+                layer = records[i].getLayer();
+                this.map.addLayer(layer);
+                if(index !== this.map.layers.length-1) {
+                    this.map.setLayerIndex(layer, index);
+                }
+            }
+            delete this._adding;
+        }
+    },
+
+    /**
+     * Handler for a store's remove event
+     * @private
+     * @param {Ext.data.Store} store
+     * @param {Ext.data.Model} record
+     * @param {Number} index
+     */
+    onRemove: function(store, record, index){
+        if(!this._removing) {
+            var layer = record.getLayer();
+            if (this.map.getLayer(layer.id) != null) {
+                this._removing = true;
+                this.removeMapLayer(record);
+                delete this._removing;
+            }
+        }
+    },
+
+    /**
+     * Handler for a store's update event
+     * @private
+     * @param {Ext.data.Store} store
+     * @param {Ext.data.Model} record
+     * @param {Number} operation
+     */
+    onUpdate: function(store, record, operation) {
+        if(operation === Ext.data.Record.EDIT) {
+            if (record.modified && record.modified.title) {
                 var layer = record.getLayer();
-                if (this.map.getLayer(layer.id) != null) {
-                    this._removing = true;
-                    this.removeMapLayer(record);
-                    delete this._removing;
+                var title = record.get("title");
+                if(title !== layer.name) {
+                    layer.setName(title);
                 }
             }
-        },
+        }
+    },
 
-        /**
-         * Handler for a store's update event
-         * @private
-         * @param {Ext.data.Store} store
-         * @param {Ext.data.Model} record
-         * @param {Number} operation
-         */
-        onUpdate: function(store, record, operation) {
-            if(operation === Ext.data.Record.EDIT) {
-                if (record.modified && record.modified.title) {
-                    var layer = record.getLayer();
-                    var title = record.get("title");
-                    if(title !== layer.name) {
-                        layer.setName(title);
-                    }
-                }
-            }
-        },
+    /**
+     * Removes a record's layer from the bound map.
+     * @private
+     * @param {Ext.data.Record} record
+     */
+    removeMapLayer: function(record){
+        this.map.removeLayer(record.getLayer());
+    },
 
-        /**
-         * Removes a record's layer from the bound map.
-         * @private
-         * @param {Ext.data.Record} record
-         */
-        removeMapLayer: function(record){
-            this.map.removeLayer(record.getLayer());
-        },
+    /**
+     * Handler for a store's data collections' replace event
+     * @private
+     * @param {String} key
+     * @param {Ext.data.Model} oldRecord In this case, a record that has
+     *     been replaced.
+     * @param {Ext.data.Model} newRecord In this case, a record that is
+     *     replacing oldRecord.
+     */
+    onReplace: function(key, oldRecord, newRecord){
+        this.removeMapLayer(oldRecord);
+    },
 
-        /**
-         * Handler for a store's data collections' replace event
-         * @private
-         * @param {String} key
-         * @param {Ext.data.Model} oldRecord In this case, a record that has
-         *     been replaced.
-         * @param {Ext.data.Model} newRecord In this case, a record that is
-         *     replacing oldRecord.
-         */
-        onReplace: function(key, oldRecord, newRecord){
-            this.removeMapLayer(oldRecord);
-        },
+    /**
+     * Get the record for the specified layer
+     * @param {OpenLayers.Layer} layer
+     * @returns {Ext.data.Model} or undefined if not found
+     */
+    getByLayer: function(layer) {
+        var index = this.findBy(function(r) {
+            return r.getLayer() === layer;
+        });
+        if(index > -1) {
+            return this.getAt(index);
+        }
+    },
 
-        /**
-         * Get the record for the specified layer
-         * @param {OpenLayers.Layer} layer
-         * @returns {Ext.data.Model} or undefined if not found
-         */
-        getByLayer: function(layer) {
-            var index = this.findBy(function(r) {
-                return r.getLayer() === layer;
-            });
-            if(index > -1) {
-                return this.getAt(index);
-            }
-        },
-
-        /**
-         * @private
-         */
-        destroy: function() {
-            var me = this;
-            me.unbind();
-            me.callParent();
-        },
+    /**
+     * @private
+     */
+    destroy: function() {
+        var me = this;
+        me.unbind();
+        me.callParent();
+    },
 
     /**
      * Build layer records out of an array of layers and appends them to the
