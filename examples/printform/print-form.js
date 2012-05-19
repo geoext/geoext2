d315abe534d12efdf3411a615ba62bc389a3cfd6
Marc Jansen <jansen@terrestris.de>
2012-05-20 00:29:21 +0200

    Point towards the github version of the license in the header.

--- examples/printform/print-form.js
+++ examples/printform/print-form.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2008-2011 The Open Source Geospatial Foundation
  * 
  * Published under the BSD license.
- * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full
+ * See https://github.com/geoext/geoext2/blob/master/license.txt for the full
  * text of the license.
  */
 
