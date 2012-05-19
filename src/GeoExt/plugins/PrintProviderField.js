ba544c70e0a3d8f4d1d0af7b43927dceba5e1832
Marc Jansen <jansen@terrestris.de>
2012-05-20 00:21:50 +0200

    Point towards the github version of the license in the header.

--- src/GeoExt/plugins/PrintProviderField.js
+++ src/GeoExt/plugins/PrintProviderField.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2008-2012 The Open Source Geospatial Foundation
  * 
  * Published under the BSD license.
- * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full
+ * See https://github.com/geoext/geoext2/blob/master/license.txttxt for the full
  * text of the license.
  */
 
