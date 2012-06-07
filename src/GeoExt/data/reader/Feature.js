4366b996981d0e203885c09ae56d207a3b61d45f
Bart van den Eijnden <bartvde@opengeo.org>
2012-06-07 21:36:38 +0200

    fix typo

--- src/GeoExt/data/reader/Feature.js
+++ src/GeoExt/data/reader/Feature.js
@@ -23,14 +23,14 @@ Ext.define('GeoExt.data.reader.Feature', {
      */
     buildExtractors: function() {
         this.callParent(arguments);
-        this.convertRecordData = this.convertFreatureRecordData;
+        this.convertRecordData = this.convertFeatureRecordData;
     },
 
     /**
      * Copy feature attribute to record.
      * @private
      */
-    convertFreatureRecordData: function(convertedValues, feature, record) {
+    convertFeatureRecordData: function(convertedValues, feature, record) {
         var records = [];
 
         if (feature) {
