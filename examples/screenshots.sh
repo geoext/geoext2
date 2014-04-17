cfb00ceb820e663f3f8b448c3a406885cce8f081
hutzelknecht <micha@immerdieses.de>
2014-04-17 09:27:31 +0200

    for loop formatted for better readability

--- examples/screenshots.sh
+++ examples/screenshots.sh
@@ -1,5 +1,9 @@
 exampleUrl = $1
 
 phantomjs screenshots.js exampleUrl
+
 # requires imagemagick
-for THUMB in $(find . | grep thumb.png); do convert -resize 118X90 $THUMB $THUMB; done
+for THUMB in $(find . | grep thumb.png) 
+do 
+  convert -resize 118X90 $THUMB $THUMB
+done
