febbcd2c4f7429bacfbeb71721b856f9b2342603
hutzelknecht <micha@immerdieses.de>
2014-04-17 11:32:03 +0200

    added check for dependent tools and support for calling script from anywhere

--- examples/screenshots.sh
+++ examples/screenshots.sh
@@ -1,9 +1,23 @@
-exampleUrl = $1
+#!/usr/bin/env bash
 
-phantomjs screenshots.js exampleUrl
+function chkcmd {
+    which $1 >/dev/null
+    if [ $? -ne 0 ];then
+        echo "Program '$1' not found."
+        exit 1
+    fi
+}
+chkcmd "phantomjs"
+chkcmd "convert"
 
-# requires imagemagick
-for THUMB in $(find . | grep thumb.png) 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+exampleUrl=$1
+
+# take screenshots - requires phantomjs
+phantomjs "$SCRIPTDIR/screenshots.js" exampleUrl
+
+# resize screenshots - requires imagemagick
+for THUMB in $(find "$SCRIPTDIR/examples" | grep thumb.png) 
 do 
   convert -resize 118X90 $THUMB $THUMB
 done
