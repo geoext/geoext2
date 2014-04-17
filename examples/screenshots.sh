6a534cc06f18061d8fbd05a211e861fddc0ee133
hutzelknecht <micha@immerdieses.de>
2014-04-17 09:19:03 +0200

    reuse variable from above

--- examples/screenshots.sh
+++ examples/screenshots.sh
@@ -1,5 +1,5 @@
 exampleUrl = $1
 
-phantomjs screenshots.js $1
+phantomjs screenshots.js exampleUrl
 # requires imagemagick
 for THUMB in $(find . | grep thumb.png); do convert -resize 118X90 $THUMB $THUMB; done
