66a2f00f9f74712d3dc1b6c599c5054b579c6c0a
Marc Jansen <jansen@terrestris.de>
2014-06-12 17:09:50 +0200

    Replace removeAllListeners w/ clearListeners.

--- src/GeoExt/FeatureRenderer.js
+++ src/GeoExt/FeatureRenderer.js
@@ -212,8 +212,8 @@ Ext.define('GeoExt.FeatureRenderer', {
      * @private
      */
     clearCustomEvents: function() {
-        if (this.el && this.el.removeAllListeners) {
-            this.el.removeAllListeners();
+        if (this.el && this.el.clearListeners) {
+            this.el.clearListeners();
         }
     },
 
