cdb75aa7777dd8012c6ff31e0cc82d64f77b0119
Marc Jansen <jansen@terrestris.de>
2014-06-12 17:16:20 +0200

    Make FeatureReader compatible w/ ExtJS 4/5.

--- examples/grid/feature-grid.js
+++ examples/grid/feature-grid.js
@@ -27,7 +27,7 @@ Ext.application({
     name: 'Feature Grid - GeoExt2',
     launch: function() {
         // create map instance
-        var map = new OpenLayers.Map();
+        var map = new OpenLayers.Map({allOverlays: true, fallThrough: true});
         var wmsLayer = new OpenLayers.Layer.WMS(
             "OpenStreetMap WMS",
             "http://ows.terrestris.de/osm/service?",
@@ -93,13 +93,14 @@ Ext.application({
                 {
                     name: 'symbolizer',
                     convert: function(v, r) {
-                        return r.raw.layer.styleMap.createSymbolizer(r.raw, 'default');
+                        var dataKey = GeoExt.isExt4 ? 'raw' : 'data',
+                            data = r[dataKey];
+                        return data.layer.styleMap.createSymbolizer(data, 'default');
                     }
                 },
                 {name: 'name', type: 'string'},
                 {name: 'elevation', type: 'float'}
-            ],
-            autoLoad: true
+            ]
         });
 
         // create grid panel configured with feature store
@@ -114,7 +115,7 @@ Ext.application({
                 width: 30,
                 xtype: 'gx_symbolizercolumn',
                 dataIndex: "symbolizer"
-            },{
+            }, {
                 header: "Name",
                 width: 200,
                 dataIndex: "name"
--- src/GeoExt/data/reader/Feature.js
+++ src/GeoExt/data/reader/Feature.js
@@ -22,18 +22,79 @@ Ext.define('GeoExt.data.reader.Feature', {
     requires: [
         'GeoExt.Version'
     ],
+
+    createFieldAccessor: function(field){
+        var accessor = this.callParent([field]);
+        if(!Ext.isDefined(accessor)) {
+            // We weren't configured with a field definition that resulted in a
+            // possible complex assessor, Let's define one where the field's
+            // name is looked up inside of the attribute-object of the
+            // OpenLayers feature.
+            //
+            // An alternative would be to define a mapping prior and then call
+            // the parents `createFieldAccessor`-methoid again...
+            //
+            // var newMapping = 'attributes.' + field.name,
+            //     newFieldDef = Ext.apply(field, {mapping: newMapping});
+            // accessor = this.callParent([newFieldDef]);
+            //
+            // ...The above code actually works as well as the line below.
+            // Internally the lines above will eventually lead to the code
+            // below.
+            accessor = this.createAccessor('attributes.' + field.name)
+        }
+        return accessor;
+    },
+
     /**
-     * Force to have our convertRecordData.
+     *
+     */
+    extractRecord: function(feature){
+        var featureState = feature && feature.state,
+            states = OpenLayers.State,
+            // newly inserted features need to be made into phantom records
+            id = (featureState === states.INSERT) ? undefined : feature.id,
+            record;
+
+        // call the parent which actually also modifies the given feature in
+        record = this.callParent(arguments);
+
+        record.state = featureState;
+        if (featureState === states.INSERT || featureState === states.UPDATE) {
+            // setDirty is marked deprecated without replacement
+            // record.setDirty();
+            // TODO below line is currently untested
+            record.dirty = true;
+        }
+        // original:
+        // convertedValues['id'] = id;
+        // TODO below line only partly tested, it currently exposes the same
+        // behaviour we had with ExtJS 4
+        //
+        // we have to store the state of 'dirty' so we can set it back after we
+        // have set the id (TODO we might need to change this to instead not use
+        // the setter at all)
+        var oldDirty = record.dirty
+        record.setId(id);
+        record.dirty = oldDirty;
+
+        return record;
+    },
+
+    /**
+     * Force to have our convertRecordData. Only needed for ExtJS 4
      *
      * @private
      */
     buildExtractors: function() {
         this.callParent(arguments);
-        this.convertRecordData = this.convertFeatureRecordData;
+        if (GeoExt.isExt4) {
+            this.convertRecordData = this.convertFeatureRecordData;
+        }
     },
 
     /**
-     * Copy feature attribute to record.
+     * Copy feature attribute to record. Only needed for ExtJS 4
      *
      * @param {Array} convertedValues
      * @param {Object} feature
@@ -41,6 +102,11 @@ Ext.define('GeoExt.data.reader.Feature', {
      * @private
      */
     convertFeatureRecordData: function(convertedValues, feature, record) {
+        //<debug>
+        if (GeoExt.isExt5) {
+            Ext.Error.raise('convertFeatureRecordData should not be called when using ExtJS 5');
+        }
+        //</debug>
         if (feature) {
             var fields = record.fields;
             var values = {};
