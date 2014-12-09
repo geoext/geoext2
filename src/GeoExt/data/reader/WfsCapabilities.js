4170bd08ba8832a97354f2c9ad1c360ea2d4b21d
Kai Volland <kaivolland@yahoo.de>
2014-12-09 16:19:44 +0100

    Fixed the test for data/reader/WfsCapabilities.js
    Adjustments to the class where needed.
    Using the config object know to get Getters and Setters.
    This is needed for ExtJS5 and also supported by ExtJS4, so we should use it.

--- src/GeoExt/data/reader/WfsCapabilities.js
+++ src/GeoExt/data/reader/WfsCapabilities.js
@@ -45,6 +45,18 @@ Ext.define('GeoExt.data.reader.WfsCapabilities', {
         }
     },
 
+    config: {
+        /**
+         *
+         */
+        layerOptions: null,
+
+        /**
+         *
+         */
+        protocolOptions: null
+    },
+
     /**
      * Gets the records.
      *
@@ -139,7 +151,7 @@ Ext.define('GeoExt.data.reader.WfsCapabilities', {
                     strategies: [new OpenLayers.Strategy.Fixed()],
                     projection: featureType.srs
                 };
-                var metaLayerOptions = this.layerOptions;
+                var metaLayerOptions = this.getLayerOptions();
                 if (metaLayerOptions) {
                     Ext.apply(layerOptions, Ext.isFunction(metaLayerOptions) ?
                         metaLayerOptions() : metaLayerOptions);
