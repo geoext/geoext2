cca8818cf7757fd751e6f2c5418c3ec2ccb18d3d
Marc Jansen <jansen@terrestris.de>
2015-01-11 11:03:38 +0100

    Always return `raw`, don't try to be too smart.

--- src/GeoExt/data/LayerModel.js
+++ src/GeoExt/data/LayerModel.js
@@ -56,6 +56,6 @@ Ext.define('GeoExt.data.LayerModel',{
      * @return {OpenLayers.Layer}
      */
     getLayer: function() {
-        return (GeoExt.isExt4) ? this.raw : this.data;
+        return this.raw;
     }
 });
