f043d6c6977e8270206b8f76c320bcfb2a650d8c
Christian Mayer <chris@meggsimum.de>
2015-11-04 14:23:40 +0100

    Set versions back to 2.1.0-dev
    After the 2.1.0-beta.3 version has been released, the version
    declarations are set back to 2.1.0-dev.

--- src/GeoExt/Version.js
+++ src/GeoExt/Version.js
@@ -9,7 +9,7 @@
     var major = 2,
         minor = 1,
         patch = 0,
-        label = 'beta.3',
+        label = 'dev',
         environment = [],
         extVersions = Ext.versions.extjs,
         isExt5 = false,
